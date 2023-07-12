47c8023bcc24ebcfdea86a6ea38f91c70d5a4996
onatege <ege.karaman@bilgiedu.net>
2023-07-12 19:05:45 +0300

    Code fixes.

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -5,9 +5,9 @@ namespace NLayer.Core.Services
     public interface IService<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
+        Task <IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
